64b28a37dfaa3f0a099b5995957f1fda970b3794
MuhammadGhazivedaBelvanaufal <qhikizaalicia@gmail.com>
2024-04-15 04:03:44 +0700

    menyelesaikan program dengan runtime configuration

---
+++ CovidConfig.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul8_1302220011
+{
+    // Kelas untuk menyimpan konfigurasi COVID-19
+    public class CovidConfig
+    {
+        public string UnitSuhu { get; set; }
+        public int MaksHariDemam { get; set; }
+        public string PesanTolak { get; set; }
+        public string PesanTerima { get; set; }
+
+        public void UbahSatuan()
+        {
+            if (UnitSuhu == "Celcius")
+                UnitSuhu = "Fahrenheit";
+            else
+                UnitSuhu = "Celcius";
+        }
+    }
+
+    // Kelas untuk mengelola konfigurasi COVID-19
+    public class PengelolaCovidConfig
+    {
+        private string lokasiFile = "config.json";
+        private CovidConfig konfigurasi;
+
+        public PengelolaCovidConfig()
+        {
+            MuatKonfigurasi();
+        }
+
+        private void MuatKonfigurasi()
+        {
+            try
+            {
+                string data = File.ReadAllText(lokasiFile);
+                konfigurasi = JsonSerializer.Deserialize<CovidConfig>(data);
+            }
+            catch
+            {
+                konfigurasi = new CovidConfig
+                {
+                    UnitSuhu = "Celcius",
+                    MaksHariDemam = 14,
+                    PesanTolak = "Maaf, Anda tidak diizinkan masuk ke gedung ini.",
+                    PesanTerima = "Silakan masuk ke gedung ini."
+                };
+                SimpanKonfigurasi();
+            }
+        }
+
+        private void SimpanKonfigurasi()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string data = JsonSerializer.Serialize(konfigurasi, options);
+            File.WriteAllText(lokasiFile, data);
+        }
+
+        public void UbahUnitSuhu()
+        {
+            konfigurasi.UbahSatuan();
+            SimpanKonfigurasi();
+        }
+
+        public CovidConfig AmbilKonfigurasi()
+        {
+            return konfigurasi;
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul8_1302220011;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PengelolaCovidConfig pengelola = new PengelolaCovidConfig();
+        CovidConfig konfigurasi = pengelola.AmbilKonfigurasi();
+
+        Console.WriteLine($"Satuan suhu saat ini adalah {konfigurasi.UnitSuhu}");
+        Console.Write("Apakah ingin mengganti satuan suhu? (y/n): ");
+        string jawabanGantiSatuan = Console.ReadLine().ToLower();
+
+        if (jawabanGantiSatuan == "y")
+        {
+            pengelola.UbahUnitSuhu();
+            konfigurasi = pengelola.AmbilKonfigurasi();
+        }
+
+        Console.Write($"Berapa suhu badan Anda saat ini? Dalam nilai {konfigurasi.UnitSuhu} : ");
+        double suhuBadan = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write($"Berapa hari yang lalu (perkiraan) Anda terakhir memiliki gejala demam? : ");
+        int hariTerakhirDemam = int.Parse(Console.ReadLine());
+
+        bool batasDemamTerima = hariTerakhirDemam < konfigurasi.MaksHariDemam;
+        bool suhuCelciusTerima = (konfigurasi.UnitSuhu == "Celcius") && (suhuBadan >= 36.5 && suhuBadan <= 37.5);
+        bool suhuFahrenheitTerima = (konfigurasi.UnitSuhu == "Fahrenheit") && (suhuBadan >= 97.7 && suhuBadan <= 99.5);
+
+        if (!batasDemamTerima || (!suhuCelciusTerima && !suhuFahrenheitTerima))
+        {
+            Console.WriteLine(konfigurasi.PesanTolak);
+        }
+        else
+        {
+            Console.WriteLine(konfigurasi.PesanTerima);
+        }
+    }
+}
